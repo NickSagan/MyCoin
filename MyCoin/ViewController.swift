1f299297996d2607cd42d6c425e76fbc9ad4ac0b
NickSagan <9952728@gmail.com>
2022-01-09 09:24:25 +0300

    add IBOutlets

--- MyCoin/ViewController.swift
+++ MyCoin/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
