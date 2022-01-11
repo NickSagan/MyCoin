21d7379b720a0bb42ad6e8b4eb4411314431e5e7
NickSagan <9952728@gmail.com>
2022-01-11 16:28:36 +0300

    add CoinManager

---
+++ MyCoin/CoinManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoinManager.swift
+//  MyCoin
+//
+//  Created by Nick Sagan on 11.01.2022.
+//
+
+import Foundation
+
+class CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC/"
+    let apiKey = "8394335E-2E9A-44EB-94C7-9060C5287800"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
