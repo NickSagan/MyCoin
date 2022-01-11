52d634903047b41b84703c44623fec38fe834ec3
NickSagan <9952728@gmail.com>
2022-01-11 16:29:27 +0300

    add func getCoinPrice()

--- MyCoin/CoinManager.swift
+++ MyCoin/CoinManager.swift
@@ -12,4 +12,10 @@ class CoinManager {
     let apiKey = "8394335E-2E9A-44EB-94C7-9060C5287800"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getCoinPrice(for currency: String) {
+        let urlString = baseURL + currency + "?apikey=" + apiKey
+        performRequest(urlString: urlString)
+    }
+    
 }
